af9236f8c2b334a6ea6158c0f6a76fcb81fcdaee
Brayden <bapricha@calpoly.edu>
2025-01-23 17:25:36 -0800

    Added backend and frontend. Starter edits.

--- expo-frontend/app/index.tsx
+++ expo-frontend/app/index.tsx
@@ -2,33 +2,19 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.default}>
+      <Text>POGGGGGGGGGGGGGG</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  mainContainer: {
+
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  default: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
 });
